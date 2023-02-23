e41f199ce93b25715de5cd8f9d2eccf05e70beb9
Hara Chung <harachung90@gmail.com>
2023-02-23 19:36:44 +1100

    Second commit. Add, Multiply view; flat HTML, simple template

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-from flask import Flask
-from flask import request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/<name>')
 def index(name="Hara"):  # put application's code here
-    name = request.args.get('name', name)
-    return "Hello from {}".format(name)
+    return render_template("index.html", name=name)
 
+
+@app.route('/add/<int:num1>/<int:num2>')
+@app.route('/add/<float:num1>/<float:num2>')
+@app.route('/add/<int:num1>/<float:num2>')
+@app.route('/add/<float:num1>/<int:num2>')
+def add(num1, num2):
+    context = {'num1': num1, 'num2': num2}
+    return render_template("add.html", **context)
 
 if __name__ == '__main__':
     app.run()
